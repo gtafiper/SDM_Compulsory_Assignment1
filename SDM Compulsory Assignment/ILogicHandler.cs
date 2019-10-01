381027b830ed8bfdb0d86b78dedbd974bda13c24
nijashansen <nijas.w.hansen@gmail.com>
2019-10-01 11:13:01 +0200

    commit

---
+++ SDM Compulsory Assignment/ILogicHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace SDM_Compulsory_Assignment
+{
+    public interface ILogicHandler
+    {
+        int getNumberOfReviewsByReviewer(int reviewer);
+    }
+}
---
+++ SDM Compulsory Assignment/LogicHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace SDM_Compulsory_Assignment
+{
+    public class LogicHandler : ILogicHandler
+    {
+        public int getNumberOfReviewsByReviewer(int reviewer)
+        {
+            return 0;
+        }
+    }
+}
